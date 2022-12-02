533ce8e88a3b6d1f3b4c22c3ea7736a032daa28e
coltoy <cmartydev@gmail.com>
2022-12-01 21:40:29 -0800

    Added initial files.

---
+++ src/main/java/com/example/circlecidemo/CircleCiDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.circlecidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCiDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CircleCiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/circlecidemo/CircleCiDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.circlecidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
